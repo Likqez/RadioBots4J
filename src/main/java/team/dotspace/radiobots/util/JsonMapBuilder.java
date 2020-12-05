e6a01ec98a010ca81a123b97cf953ce98ef9695a
Lukas Lanzner <lanzner09@gmail.com>
2020-12-05 04:11:57 +0100

    added JsonMapBuilder.java

---
+++ src/main/java/team/dotspace/radiobots/util/JsonMapBuilder.java
@@ -0,0 +1,26 @@
+package team.dotspace.radiobots.util;
+
+import kong.unirest.JsonObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonMapBuilder {
+
+    private final HashMap<String, Object> map = new HashMap<>();
+
+    public JsonMapBuilder addEntry(String key, Object value) {
+        this.map.put(key, value);
+        return this;
+    }
+
+    public JsonMapBuilder addEntry(Map<String, ?> map) {
+        this.map.putAll(map);
+        return this;
+    }
+
+    public String build() {
+        return new JsonObjectMapper().writeValue(map);
+    }
+
+}
