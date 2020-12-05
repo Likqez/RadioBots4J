074b754c914877753f9373d1180af58a356dab05
Lukas Lanzner <lanzner09@gmail.com>
2020-12-05 04:59:21 +0100

    implemented get/creation/deletion methods for cloudservers

--- src/main/java/team/dotspace/radiobots4j/category/CloudCategory.java
+++ src/main/java/team/dotspace/radiobots4j/category/CloudCategory.java
@@ -13,6 +13,8 @@ public class CloudCategory {
         this.provider = provider;
     }
 
+    /* Misc */
+
     /**
      * @apiNote <a href="https://docs.radiobots.eu/#cloud-get-all-regions">https://docs.radiobots.eu/#cloud-get-all-regions</a>
      */
@@ -24,6 +26,63 @@ public class CloudCategory {
      * @apiNote <a href="https://docs.radiobots.eu/#cloud-get-a-task">https://docs.radiobots.eu/#cloud-get-a-task</a>
      */
     public HttpResponse<JsonNode> getTask(String uuid) {
-        return provider.get("cloud/task").header("Data", new JsonMapBuilder().addEntry("uuid", uuid).build()).asJson();
+        return provider.get("cloud/task")
+                .header("Data", new JsonMapBuilder().addEntry("uuid", uuid).build())
+                .asJson();
+    }
+
+    /* CloudServer */
+
+    /**
+     * @apiNote <a href="https://docs.radiobots.eu/#cloud-cloudserver-create-a-cloudserver">https://docs.radiobots.eu/#cloud-cloudserver-create-a-cloudserver</a>
+     */
+    public HttpResponse<JsonNode> createServer(String name, String region, String series, String image, int cores, double ram, int disk, String network, String ssh_key) {
+        network = "both";
+        return provider.post("cloud/cloudserver/server").header("Data",
+                new JsonMapBuilder()
+                        .addEntry("name", name)
+                        .addEntry("region", region)
+                        .addEntry("series", series)
+                        .addEntry("image", image)
+                        .addEntry("cores", cores)
+                        .addEntry("ram", ram)
+                        .addEntry("disk", disk)
+                        .addEntry("network", network)
+                        .addEntry("ssh_keys", ssh_key)
+                        .addEntry("calc", false)
+                        .build()
+        ).asJson();
+    }
+
+    /**
+     * @apiNote <a href="https://docs.radiobots.eu/#cloud-cloudserver-delete-a-cloudserver">https://docs.radiobots.eu/#cloud-cloudserver-delete-a-cloudserver</a>
+     */
+    public HttpResponse<JsonNode> deleteServer(String uuid) {
+        return provider.delete("cloud/cloudserver/server")
+                .header("Data", new JsonMapBuilder().addEntry("uuid", uuid).build())
+                .asJson();
+    }
+
+    /**
+     * @apiNote <a href="https://docs.radiobots.eu/#cloud-cloudserver-get-all-cloudserver">https://docs.radiobots.eu/#cloud-cloudserver-get-all-cloudserver</a>
+     */
+    public HttpResponse<JsonNode> getServers() {
+        return provider.get("cloud/cloudserver/get").asJson();
+    }
+
+    /**
+     * @apiNote <a href="https://docs.radiobots.eu/#cloud-cloudserver-get-a-cloudserver">https://docs.radiobots.eu/#cloud-cloudserver-get-a-cloudserver</a>
+     */
+    public HttpResponse<JsonNode> getServer(String uuid) {
+        return provider.get("cloud/cloudserver/server")
+                .header("Data", new JsonMapBuilder().addEntry("uuid", uuid).build())
+                .asJson();
+    }
+
+    /**
+     * @apiNote <a href="https://docs.radiobots.eu/#cloud-cloudserver-get-all-images">https://docs.radiobots.eu/#cloud-cloudserver-get-all-images</a>
+     */
+    public HttpResponse<JsonNode> getImages() {
+        return provider.get("cloud/cloudserver/images").asJson();
     }
 }
